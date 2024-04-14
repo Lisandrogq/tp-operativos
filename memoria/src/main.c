bcd573d8cd0eecbc513a8f15d22bc94f3e436485
AhoraNoSoyTobi <lisandrogq7@gmail.com>
2024-04-14 14:03:01 +0000

    servidor de modulo memoria(parcial)

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,8 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/hello.c>
+#include <string.h>
+#include <commons/log.h>
+#include "utils.h"
 
-int main(int argc, char* argv[]) {
-    decir_hola("Memoria");
+int main(int argc, char *argv[])
+{
+    logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
+    int server_fd = iniciar_servidor();
+    log_info(logger, "Memoria lista para recibir");
+    int cliente_fd = esperar_cliente(server_fd);
+
+    recibir_operacion(cliente_fd);
+    handshake(cliente_fd);
+
+    while (1)
+    {
+
+		int cod_op = recibir_operacion(cliente_fd);
+        switch (cod_op)
+        {
+        case MENSAJE:
+			recibir_mensaje(cliente_fd);
+            break;
+        case -1:
+            log_error(logger, "el cliente se desconecto. Terminando servidor");
+            return EXIT_FAILURE;
+        default:
+            log_warning(logger, "Operacion desconocida. No quieras meter la pata");
+            break;
+        }
+    }
+
+    decir_hola("Memoriaa");
     return 0;
 }
---
+++ memoria/src/utils.c
@@ -0,0 +1,79 @@
+#include"utils.h"
+
+t_log* logger;
+
+
+int iniciar_servidor(void)
+{
+	int socket_servidor;
+
+	struct addrinfo hints, *server_info;//, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, PUERTO, &hints, &server_info);
+
+	// Creamos el socket de escucha del servidor
+	socket_servidor = socket(server_info->ai_family,server_info->ai_socktype,server_info->ai_protocol);
+
+	// Asociamos el socket a un puerto
+	bind(socket_servidor, server_info->ai_addr, server_info->ai_addrlen);
+	// Escuchamos las conexiones entrantes
+	listen(socket_servidor, SOMAXCONN);
+	freeaddrinfo(server_info);
+	log_info(logger, "Listo para escuchar a otro modulo"); //El profe puse log_trace
+
+	return socket_servidor;
+}
+
+int esperar_cliente(int socket_servidor)
+{
+	// Aceptamos un nuevo cliente
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
+	log_info(logger, "Se conecto otro modulo!");
+
+	return socket_cliente;
+}
+
+int recibir_operacion(int socket_cliente)
+{
+	int cod_op;
+	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0){
+		return cod_op;}
+	else
+	{
+		close(socket_cliente);
+		return -1;
+	}
+}
+
+void handshake(int socket_cliente)
+{
+	int size;
+	char* buffer = recibir_buffer(&size, socket_cliente);
+	log_info(logger, "Handshake con: %s", buffer);
+	free(buffer);
+}
+
+void recibir_mensaje(int socket_cliente)
+{
+	int size;
+	char* buffer = recibir_buffer(&size, socket_cliente);
+	log_info(logger, "Me llego el mensaje %s", buffer);
+	free(buffer);
+}
+
+
+void* recibir_buffer(int* size, int socket_cliente)
+{
+	void * buffer;
+
+	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}
