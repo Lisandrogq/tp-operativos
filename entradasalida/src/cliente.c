c31bab6af58f2279c25d4023f36a9fba0cb7e42d
Santiago Podesta <santi.podesta.c@gmail.com>
2024-05-23 15:16:57 +0000

    hilos para las conexiones en entrada/salida

--- entradasalida/src/cliente.c
+++ entradasalida/src/cliente.c
@@ -1,42 +1,74 @@
 #include "client.h"
 #include <readline/readline.h>
 
-int main(void)
+t_log *logger;
+t_config *config;
+pthread_t tid[2];
+
+int inicializar_cliente_memoria() // todavia no se usa
 {
 	int conexion_fd;
-	char* ip;
-	char* puerto;
-	char* modulo;
+	char *ip;
+	char *puerto;
+	char *modulo;
 
-	t_log* logger;
-	t_config* config;
-
-	logger = iniciar_logger();
-	
-	logger = log_create("IO.log","IO_MateLavado",1,LOG_LEVEL_INFO);
-	log_info(logger,"Soy un log!");
-
-
-	config = iniciar_config();
-	config = config_create("IO.config");
 	modulo = config_get_string_value(config, "MODULO");
-	log_info(logger,modulo);
-
+	log_info(logger, "Este es el modulo: %s", modulo);
 	ip = config_get_string_value(config, "IP_MEMORIA");
 	puerto = config_get_string_value(config, "PUERTO_MEMORIA");
-	//ip = config_get_string_value(config, "IP_KERNEL");
-	//puerto = config_get_string_value(config, "PUERTO_KERNEL");
-	
-	conexion_fd = crear_conexion(ip, puerto,logger);
-	int resultado = handshake(conexion_fd);
-	
-	enviar_operacion(OPERACION_IO_1,modulo, conexion_fd); // modulo es la info que se mand
-	enviar_operacion(MENSAJE,"segundo mensaje", conexion_fd);
-	//intento de crear interfaz generica
-	
-	sleep(5); //para simular varios clientes simultaneamente
 
-	terminar_programa(conexion_fd, logger, config);
+	conexion_fd = crear_conexion(ip, puerto, logger);
+	int resultado = handshake(conexion_fd);
+	if (resultado == -1)
+		return;
+	
+	return conexion_fd;
+}
+int inicializar_cliente_kernel()
+{
+	int conexion_fd;
+	char *ip;
+	char *puerto;
+	char *modulo;
+
+	modulo = config_get_string_value(config, "MODULO");
+	log_info(logger, "Este es el modulo: %s", modulo);
+	ip = config_get_string_value(config, "IP_KERNEL");
+	puerto = config_get_string_value(config, "PUERTO_KERNEL");
+
+	conexion_fd = crear_conexion(ip, puerto, logger);
+	int resultado = handshake(conexion_fd);
+	if (resultado == -1)
+		return;
+	
+	return conexion_fd;
+}
+
+int main(void)
+{
+	logger = iniciar_logger();
+	logger = log_create("IO.log","IO_MateLavado",1,LOG_LEVEL_INFO);
+	config = iniciar_config();
+	config = config_create("IO.config");
+	int err;
+	err = pthread_create(&(tid[0]), NULL, inicializar_cliente_memoria, NULL); // todavia no se usa
+	if (err != 0)
+	{
+		log_error(logger, "Hubo un problema al crear el thread cliente-memoria:[%s]", strerror(err));
+		return err;
+	}
+	log_info(logger, "El thread cliente-memoria inició su ejecución");
+	err = pthread_create(&(tid[1]), NULL, inicializar_cliente_kernel, NULL);
+	if (err != 0)
+	{
+		log_error(logger, "Hubo un problema al crear el thread cliente-kernel:[%s]", strerror(err));
+		return err;
+	}
+	log_info(logger, "El thread cliente-kernel inició su ejecución");
+
+	pthread_join(tid[0], NULL); //join para que no termine el main creo que puede llegar a terminar igual
+	pthread_join(tid[1], NULL);
+	
 
 }
 
